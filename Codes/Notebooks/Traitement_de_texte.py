e01b49c2049be4a4eb273aceba8de5ca8134c97b
Mouhamed Seck <seckmouhamet125@gmail.com>
2025-05-08 08:12:20 +0000

    Add files via upload

---
+++ Codes/Notebooks/Traitement_de_texte.py
@@ -0,0 +1,335 @@
+# helpers_strings.py
+
+import re
+import string
+import emoji
+import unicodedata
+import spacy
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# Charger spaCy modèle
+nlp = spacy.load("en_core_web_sm")
+
+# Nettoyage simple
+def remove_bad_characters(text):
+    if isinstance(text, str):
+        return text.encode('ascii', errors='ignore').decode('utf-8')
+    return text
+
+def remove_accents(text):
+    return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+
+# Remover d'emojis version pipeline-compatible
+class EmojiRemover(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return [self.remove_emojis(text) for text in X]
+
+    def remove_emojis(self, text):
+        if not isinstance(text, str):
+            return text
+        matches = emoji.emoji_list(text)
+        for match in matches:
+            emoji_char = match["emoji"]
+            text = text.replace(emoji_char, '')
+        return text
+
+# Dictionnaire d'abréviations
+abbreviations = {
+    "$" : " dollar ",
+    "€" : " euro ",
+    "4ao" : "for adults only",
+    "a.m" : "before midday",
+    "a3" : "anytime anywhere anyplace",
+    "aamof" : "as a matter of fact",
+    "acct" : "account",
+    "adih" : "another day in hell",
+    "afaic" : "as far as i am concerned",
+    "afaict" : "as far as i can tell",
+    "afaik" : "as far as i know",
+    "afair" : "as far as i remember",
+    "afk" : "away from keyboard",
+    "app" : "application",
+    "approx" : "approximately",
+    "apps" : "applications",
+    "asap" : "as soon as possible",
+    "asl" : "age, sex, location",
+    "atk" : "at the keyboard",
+    "ave." : "avenue",
+    "aymm" : "are you my mother",
+    "ayor" : "at your own risk", 
+    "b&b" : "bed and breakfast",
+    "b+b" : "bed and breakfast",
+    "b.c" : "before christ",
+    "b2b" : "business to business",
+    "b2c" : "business to customer",
+    "b4" : "before",
+    "b4n" : "bye for now",
+    "b@u" : "back at you",
+    "bae" : "before anyone else",
+    "bak" : "back at keyboard",
+    "bbbg" : "bye bye be good",
+    "bbc" : "british broadcasting corporation",
+    "bbias" : "be back in a second",
+    "bbl" : "be back later",
+    "bbs" : "be back soon",
+    "be4" : "before",
+    "bfn" : "bye for now",
+    "blvd" : "boulevard",
+    "bout" : "about",
+    "brb" : "be right back",
+    "bros" : "brothers",
+    "brt" : "be right there",
+    "bsaaw" : "big smile and a wink",
+    "btw" : "by the way",
+    "bwl" : "bursting with laughter",
+    "c/o" : "care of",
+    "cet" : "central european time",
+    "cf" : "compare",
+    "cia" : "central intelligence agency",
+    "csl" : "can not stop laughing",
+    "cu" : "see you",
+    "cul8r" : "see you later",
+    "cv" : "curriculum vitae",
+    "cwot" : "complete waste of time",
+    "cya" : "see you",
+    "cyt" : "see you tomorrow",
+    "dae" : "does anyone else",
+    "dbmib" : "do not bother me i am busy",
+    "diy" : "do it yourself",
+    "dm" : "direct message",
+    "dwh" : "during work hours",
+    "e123" : "easy as one two three",
+    "eet" : "eastern european time",
+    "eg" : "example",
+    "embm" : "early morning business meeting",
+    "encl" : "enclosed",
+    "encl." : "enclosed",
+    "etc" : "and so on",
+    "faq" : "frequently asked questions",
+    "fawc" : "for anyone who cares",
+    "fb" : "facebook",
+    "fc" : "fingers crossed",
+    "fig" : "figure",
+    "fimh" : "forever in my heart", 
+    "ft." : "feet",
+    "ft" : "featuring",
+    "ftl" : "for the loss",
+    "ftw" : "for the win",
+    "fwiw" : "for what it is worth",
+    "fyi" : "for your information",
+    "g9" : "genius",
+    "gahoy" : "get a hold of yourself",
+    "gal" : "get a life",
+    "gcse" : "general certificate of secondary education",
+    "gfn" : "gone for now",
+    "gg" : "good game",
+    "gl" : "good luck",
+    "glhf" : "good luck have fun",
+    "gmt" : "greenwich mean time",
+    "gmta" : "great minds think alike",
+    "gn" : "good night",
+    "g.o.a.t" : "greatest of all time",
+    "goat" : "greatest of all time",
+    "goi" : "get over it",
+    "gps" : "global positioning system",
+    "gr8" : "great",
+    "gratz" : "congratulations",
+    "gyal" : "girl",
+    "h&c" : "hot and cold",
+    "hp" : "horsepower",
+    "hr" : "hour",
+    "hrh" : "his royal highness",
+    "ht" : "height",
+    "ibrb" : "i will be right back",
+    "ic" : "i see",
+    "icq" : "i seek you",
+    "icymi" : "in case you missed it",
+    "idc" : "i do not care",
+    "idgadf" : "i do not give a damn fuck",
+    "idgaf" : "i do not give a fuck",
+    "idk" : "i do not know",
+    "ie" : "that is",
+    "i.e" : "that is",
+    "ifyp" : "i feel your pain",
+    "IG" : "instagram",
+    "iirc" : "if i remember correctly",
+    "ilu" : "i love you",
+    "ily" : "i love you",
+    "imho" : "in my humble opinion",
+    "imo" : "in my opinion",
+    "imu" : "i miss you",
+    "iow" : "in other words",
+    "irl" : "in real life",
+    "j4f" : "just for fun",
+    "jic" : "just in case",
+    "jk" : "just kidding",
+    "jsyk" : "just so you know",
+    "l8r" : "later",
+    "lb" : "pound",
+    "lbs" : "pounds",
+    "ldr" : "long distance relationship",
+    "lmao" : "laugh my ass off",
+    "lmfao" : "laugh my fucking ass off",
+    "lol" : "laughing out loud",
+    "ltd" : "limited",
+    "ltns" : "long time no see",
+    "m8" : "mate",
+    "mf" : "motherfucker",
+    "mfs" : "motherfuckers",
+    "mfw" : "my face when",
+    "mofo" : "motherfucker",
+    "mph" : "miles per hour",
+    "mr" : "mister",
+    "mrw" : "my reaction when",
+    "ms" : "miss",
+    "mte" : "my thoughts exactly",
+    "nagi" : "not a good idea",
+    "nbc" : "national broadcasting company",
+    "nbd" : "not big deal",
+    "nfs" : "not for sale",
+    "ngl" : "not going to lie",
+    "nhs" : "national health service",
+    "nrn" : "no reply necessary",
+    "nsfl" : "not safe for life",
+    "nsfw" : "not safe for work",
+    "nth" : "nice to have",
+    "nvr" : "never",
+    "nyc" : "new york city",
+    "oc" : "original content",
+    "og" : "original",
+    "ohp" : "overhead projector",
+    "oic" : "oh i see",
+    "omdb" : "over my dead body",
+    "omg" : "oh my god",
+    "omw" : "on my way",
+    "p.a" : "per annum",
+    "p.m" : "after midday",
+    "pm" : "prime minister",
+    "poc" : "people of color",
+    "pov" : "point of view",
+    "pp" : "pages",
+    "ppl" : "people",
+    "prw" : "parents are watching",
+    "ps" : "postscript",
+    "pt" : "point",
+    "ptb" : "please text back",
+    "pto" : "please turn over",
+    "qpsa" : "what happens", #"que pasa",
+    "ratchet" : "rude",
+    "rbtl" : "read between the lines",
+    "rlrt" : "real life retweet", 
+    "rofl" : "rolling on the floor laughing",
+    "roflol" : "rolling on the floor laughing out loud",
+    "rotflmao" : "rolling on the floor laughing my ass off",
+    "rt" : "retweet",
+    "ruok" : "are you ok",
+    "sfw" : "safe for work",
+    "sk8" : "skate",
+    "smh" : "shake my head",
+    "sq" : "square",
+    "srsly" : "seriously", 
+    "ssdd" : "same stuff different day",
+    "tbh" : "to be honest",
+    "tbs" : "tablespooful",
+    "tbsp" : "tablespooful",
+    "tfw" : "that feeling when",
+    "thks" : "thank you",
+    "tho" : "though",
+    "thx" : "thank you",
+    "tia" : "thanks in advance",
+    "til" : "today i learned",
+    "tl;dr" : "too long i did not read",
+    "tldr" : "too long i did not read",
+    "tmb" : "tweet me back",
+    "tntl" : "trying not to laugh",
+    "ttyl" : "talk to you later",
+    "u" : "you",
+    "u2" : "you too",
+    "u4e" : "yours for ever",
+    "utc" : "coordinated universal time",
+    "w/" : "with",
+    "w/o" : "without",
+    "w8" : "wait",
+    "wassup" : "what is up",
+    "wb" : "welcome back",
+    "wtf" : "what the fuck",
+    "wtg" : "way to go",
+    "wtpa" : "where the party at",
+    "wuf" : "where are you from",
+    "wuzup" : "what is up",
+    "wywh" : "wish you were here",
+    "yd" : "yard",
+    "ygtr" : "you got that right",
+    "ynk" : "you never know",
+    "zzz" : "sleeping bored and tired"
+}
+
+
+def replace_abbreviations(text):
+    if not isinstance(text, str):
+        return text
+    def replace(match):
+        return abbreviations.get(match.group(0).lower(), match.group(0))
+    pattern = r'\b(?:' + '|'.join(map(re.escape, abbreviations.keys())) + r')\b'
+    return re.sub(pattern, replace, text)
+
+# Normalisation complète
+def normalize_text(text):
+    if not isinstance(text, str):
+        return text
+    text = remove_bad_characters(text)
+    text = EmojiRemover().remove_emojis(text)  # <-- appeler la méthode directement pour un texte
+    text = re.sub(r'https?:\/\/\S+', '[url]', text)
+    text = re.sub(r'@\w+', '[ment]', text)
+    text = re.sub(r'#\w+', '[hash]', text)
+    text = re.sub(r'#', ' ', text)
+    text = re.sub(r'\d+', '[number]', text)
+    text = re.sub(rf"[{re.escape(string.punctuation)}]", "", text)
+    text = re.sub(r'[-+_¡¿]', ' ', text)
+    text = remove_accents(text)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip().lower()
+
+# NLP functions
+def tokenize_text(text):
+    doc = nlp(text)
+    return [token.text for token in doc]
+
+def lemmatize_text(text):
+    doc = nlp(text)
+    return [token.lemma_ for token in doc]
+
+def remove_stopwords(text):
+    doc = nlp(text)
+    return [token.text for token in doc if not token.is_stop]
+
+def extract_named_entities(text):
+    doc = nlp(text)
+    return [ent.text for ent in doc.ents]
+
+# Pipeline final
+def preprocess_text(texts):
+    # Si `texts` est une série ou un tableau, appliquer la transformation élément par élément
+    return [single_preprocess(t) for t in texts]
+
+def single_preprocess(text):
+    text = normalize_text(text)
+    text = replace_abbreviations(text)
+    tokens = tokenize_text(text)
+    lemmatized_tokens = lemmatize_text(" ".join(tokens))
+    clean_tokens = remove_stopwords(" ".join(lemmatized_tokens))
+    return " ".join(clean_tokens)
+
+
+
+
+
+
+
+
+
+
+
